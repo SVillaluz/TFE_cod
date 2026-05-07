205ff87276c97e5696a4890a0cf242a5e9094f2c
CarlosEduardo-Gutierrez <gutierrezcortescarloseduardo@gmail.com>
2026-05-06 20:02:09 -0600

    Estructura inicial del backend

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express(); 
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Backend del Hotel funcionando!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
